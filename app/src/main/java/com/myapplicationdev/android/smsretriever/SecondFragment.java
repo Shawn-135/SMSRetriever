8cc8a34ee855fe571998866ec62ccf6f84e09325
Mathias-Andre <19031965@myrp.edu.sg>
2021-05-20 15:23:32 +0800

    SecondFragment.java done

--- app/src/main/java/com/myapplicationdev/android/smsretriever/SecondFragment.java
+++ app/src/main/java/com/myapplicationdev/android/smsretriever/SecondFragment.java
@@ -1,64 +1,75 @@
 package com.myapplicationdev.android.smsretriever;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SecondFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SecondFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SecondFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SecondFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SecondFragment newInstance(String param1, String param2) {
-        SecondFragment fragment = new SecondFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    TextView tvFrag2, tvSMS;
+    EditText etSMS;
+    Button btnRetrieveSMS;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        btnRetrieveSMS.findViewById(R.id.btnRetrieveSMS);
+        tvSMS.findViewById(R.id.tvSMS);
+        etSMS.findViewById(R.id.etSMS);
+        tvFrag2.findViewById(R.id.tvFrag2);
+
+        btnRetrieveSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String text = etSMS.getText().toString();
+                Uri uri = Uri.parse("content://sms");
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
+
+                ContentResolver cr = getActivity().getContentResolver();
+
+                String filter ="body LIKE ?";
+                String[] filterArgs = {"%" + text + "%"};
+
+                // Fetch SMS Message from Built-in Content Provider
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+                String smsBody = "";
+                if (cursor.moveToFirst()) {
+                    do {
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat.format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")) {
+                            type = "Inbox:";
+                        } else {
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date
+                                + "\n\"" + body + "\"\n\n";
+                    } while (cursor.moveToNext());
+                }
+                tvSMS.setText(smsBody);
+
+            }
+        });
+
+        return view;
     }
 }
